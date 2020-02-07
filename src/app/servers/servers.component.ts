f27835248355cd1ec2fd5c16b356ce16b422cf04
ManideepInnamuri <56725653+ManideepInnamuri@users.noreply.github.com>
2020-02-07 11:07:25 +0530

    Two Way Data Binding
    Two Way Data Binding
    in Html :+1: <input
    type="text"
    class="form-control"
    [(ngModel)]="serverName">
    in Type script assign value to serverName

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = true;
   serverCreationStatus = 'No Server was Created!!!'
-  serverName='';
+  serverName='Test Server';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = false;
