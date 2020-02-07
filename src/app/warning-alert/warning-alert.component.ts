b081dff43611805c3aab1dd9d2dc9895b3bc7ee0
ManideepInnamuri <56725653+ManideepInnamuri@users.noreply.github.com>
2020-02-07 10:13:21 +0530

    Components 1st Assignment
    Components 1st Assignment
    1. Warning Alert
    2. Success Alert

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+    selector:"app-warning-alert",
+    templateUrl:"warning-alert.component.html",
+    styleUrls:['warning-alert.component.css']
+    
+})
+export class WarningAlertComponent{
+
+}
